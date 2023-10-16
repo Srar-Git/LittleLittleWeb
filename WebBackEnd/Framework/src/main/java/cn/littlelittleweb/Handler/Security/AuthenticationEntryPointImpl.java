a401dc60755d6cad36e1b0ba93a5ccd0662c0d6c
srar-mac <1214090013@qq.com>
2023-10-16 14:58:02 +0800

    update

--- WebBackEnd/Framework/src/main/java/cn/littlelittleweb/Handler/Security/AuthenticationEntryPointImpl.java
+++ WebBackEnd/Framework/src/main/java/cn/littlelittleweb/Handler/Security/AuthenticationEntryPointImpl.java
@@ -1,18 +1,13 @@
 package cn.littlelittleweb.Handler.Security;
 
 import com.alibaba.fastjson.JSON;
-import com.sangeng.domain.ResponseResult;
-import com.sangeng.enums.AppHttpCodeEnum;
-import com.sangeng.utils.WebUtils;
+
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.InsufficientAuthenticationException;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
 
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 @Component
