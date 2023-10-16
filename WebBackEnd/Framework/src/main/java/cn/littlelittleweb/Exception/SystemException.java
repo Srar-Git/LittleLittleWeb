bb7873c73e4ddf7a9af00321c55a430813150953
srar-mac <1214090013@qq.com>
2023-10-16 15:10:51 +0800

    update

---
+++ WebBackEnd/Framework/src/main/java/cn/littlelittleweb/Exception/SystemException.java
@@ -0,0 +1,29 @@
+package cn.littlelittleweb.Exception;
+
+
+import cn.littlelittleweb.Enums.AppHttpCodeEnum;
+
+/**
+ * @Author pyf
+ */
+public class SystemException extends RuntimeException{
+
+    private int code;
+
+    private String msg;
+
+    public int getCode() {
+        return code;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public SystemException(AppHttpCodeEnum httpCodeEnum) {
+        super(httpCodeEnum.getMsg());
+        this.code = httpCodeEnum.getCode();
+        this.msg = httpCodeEnum.getMsg();
+    }
+
+}
--- WebBackEnd/Web/src/main/java/cn/littlelittleweb/Controller/UserController/UserLoginController.java
+++ WebBackEnd/Web/src/main/java/cn/littlelittleweb/Controller/UserController/UserLoginController.java
@@ -3,7 +3,9 @@ package cn.littlelittleweb.Controller.UserController;
 import cn.littlelittleweb.Domain.Entity.UserEntity.User;
 import cn.littlelittleweb.Domain.ResponseResult;
 import cn.littlelittleweb.Service.UserService.UserLoginService;
+import org.apache.poi.util.StringUtil;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,6 +20,11 @@ public class UserLoginController {
 
     @PostMapping("/login")
     public ResponseResult login(@RequestBody User user){
+        if(!StringUtils.hasText(user.getUserName())){
+            //提示： 必须要传用户
+//            throw new RuntimeException()
+        }
+
         return userLoginService.login(user);
     }
 
