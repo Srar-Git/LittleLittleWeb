876b1acb0ef6a9ce6737723eac99a3ccfadf2b28
srar-mac <1214090013@qq.com>
2023-10-16 15:00:34 +0800

    update

---
+++ WebBackEnd/Framework/src/main/java/cn/littlelittleweb/Handler/Security/AccessDeniedHandlerImpl.java
@@ -0,0 +1,26 @@
+package cn.littlelittleweb.Handler.Security;
+
+import cn.littlelittleweb.Domain.ResponseResult;
+import cn.littlelittleweb.Enums.AppHttpCodeEnum;
+import cn.littlelittleweb.Utils.WebUtils;
+import com.alibaba.fastjson.JSON;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+public class AccessDeniedHandlerImpl implements AccessDeniedHandler {
+    @Override
+    public void handle(HttpServletRequest request, HttpServletResponse response, AccessDeniedException accessDeniedException) throws IOException, ServletException {
+        accessDeniedException.printStackTrace();
+        ResponseResult result = ResponseResult.errorResult(AppHttpCodeEnum.NO_OPERATOR_AUTH);
+        //响应给前端
+        WebUtils.renderString(response, JSON.toJSONString(result));
+    }
+}
